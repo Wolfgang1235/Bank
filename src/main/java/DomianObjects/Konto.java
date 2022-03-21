d4502a136d82d681674773cc8ba04219e1ec6326
Andreas Fritzbøger <andreasfritzboger@live.dk>
2022-03-21 12:57:23 +0100

    test tilføjet i KontoTest

---
+++ src/main/java/DomianObjects/Konto.java
@@ -0,0 +1,45 @@
+package DomianObjects;
+
+public class Konto {
+    private String navn;
+    private String kode;
+    private int saldo;
+
+    public Konto(String navn, String kode, int saldo) {
+        this.navn = navn;
+        this.kode = kode;
+        this.saldo = saldo;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+
+    public int indsæt(int i) {
+//        if (i > 0) {
+//        saldo = saldo + i;
+//      }
+//        return saldo;
+        return (i>0) ? saldo+i : saldo;
+    }
+
+    public int hæv(int i) {
+        if (i > saldo) {
+            return saldo;
+        }
+
+        if (i > 0) {
+            saldo = saldo - i;
+        }
+        return saldo;
+    }
+}
---
+++ src/test/java/DomianObjects/KontoTest.java
@@ -0,0 +1,37 @@
+package DomianObjects;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KontoTest {
+
+    Konto konto;
+
+    @BeforeEach
+    void setUp() {
+        konto = new Konto("and","c00l",100);
+    }
+
+    @Test
+    void indsæt() {
+        assertEquals(200,konto.indsæt(100));
+    }
+
+    @Test
+    void indsætNegativ() {
+        assertEquals(100,konto.indsæt(-100));
+    }
+
+    @Test
+    void hæv() {
+        assertEquals(50,konto.hæv(50));
+        assertEquals(50,konto.hæv(100));
+    }
+
+    @Test
+    void hævNegativ() {
+        assertEquals(100,konto.hæv(-100));
+    }
+}
